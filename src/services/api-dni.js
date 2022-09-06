dae845f95b7c2f526c33136f131596b7378c4588
XantX <hp_xantx@hotmail.com>
2022-09-05 22:58:55 -0500

    add headers

--- src/services/api-dni.js
+++ src/services/api-dni.js
@@ -3,14 +3,17 @@ import axios from "axios";
 
 const headers = {
   "Content-Type": "application/json",
-  Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyNDg3LCJjb3JyZW8iOiJ0aGUubWF4d2VsbDg5QGdtYWlsLmNvbSIsImlhdCI6MTY2MjE1Nzg3N30.IZhKxvJHHVLeXpRfoE9cUKT2zqS58lFDHJ1IK6lJN1g'}`,
 };
 
 export const exists_DNI = (json_data) => {
   return axios
-    .post(`${'https://www.softwarelion.xyz/api/reniec/reniec-dni'}`, json_data, {
-      headers: headers,
-    })
+    .post(
+      `${"https://www.softwarelion.xyz/api/reniec/reniec-dni"}`,
+      json_data,
+      {
+        headers: headers,
+      }
+    )
     .then((response) => {
       const { data } = response;
       return data;
--- src/services/api-nlp.js
+++ src/services/api-nlp.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 //import { API_DNI_URL, www.softwarelion.xyz/api/reniec/reniec-dni} from "@env";
 
+const headers = {
+  "Content-Type": "application/json",
+};
 export default request_api = (json_data) => {
   return axios
-    .post(`${"https://api-nlp-pry20220112.herokuapp.com/spacy/"}`, {
-      texto: json_data,
-    })
+    .post(
+      `${"https://api-nlp-pry20220112.herokuapp.com/spacy/"}`,
+      {
+        texto: json_data,
+      },
+      headers
+    )
     .then((response) => {
       const { data } = response;
       console.log(data);
