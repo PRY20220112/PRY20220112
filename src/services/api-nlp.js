b9b99cd2c4592aa519237181f90bc0aff1967f6e
XantX <hp_xantx@hotmail.com>
2022-09-05 22:30:14 -0500

    Muestra de post

---
+++ src/services/api-nlp.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+//import { API_DNI_URL, www.softwarelion.xyz/api/reniec/reniec-dni} from "@env";
+
+export const exists_DNI = (json_data) => {
+  return axios
+    .post(`${"https://api-nlp-pry20220112.herokuapp.com/spacy/"}`, {
+      texto: json_data,
+    })
+    .then((response) => {
+      const { data } = response;
+      console.log(data);
+      return data;
+    });
+};
